b24b475d60edbf3bca0326872e8c422da5c59753
Rifki.Nurfaiz <rifki.nurfaiz@btpn.com>
2021-06-06 14:23:55 +0700

    Refactor rendering

--- src/Containers/HistoryTabDetail/HistoryTabDetail.component.js
+++ src/Containers/HistoryTabDetail/HistoryTabDetail.component.js
@@ -4,18 +4,136 @@ import Swiper from 'react-native-swiper';
 
 import HistoryTabDetailContent from '../HistoryTabDetailContent';
 
+const data = [
+  {
+    year: 2020,
+    data: [
+      {
+        month: 'January',
+        accounts: [
+          {
+            id: 'jenius',
+            name: 'Jenius',
+            amount: 'Rp10.000.000'
+          },
+          {
+            id: 'bca',
+            name: 'BCA',
+            amount: 'Rp10.200.140.234'
+          },
+          {
+            id: 'ovo',
+            name: 'OVO',
+            amount: 'Rp2.040.000'
+          },
+          {
+            id: 'gopay',
+            name: 'Gopay',
+            amount: 'Rp1.000'
+          }
+        ],
+        totalAmount: 'Rp212.181.234'
+      },
+      {
+        month: 'February',
+        accounts: [
+          {
+            id: 'jenius',
+            name: 'Jenius',
+            amount: 'Rp10.000.000'
+          },
+          {
+            id: 'bca',
+            name: 'BCA',
+            amount: 'Rp10.200.140.234'
+          },
+          {
+            id: 'ovo',
+            name: 'OVO',
+            amount: 'Rp2.040.000'
+          },
+          {
+            id: 'gopay',
+            name: 'Gopay',
+            amount: 'Rp1.000'
+          }
+        ],
+        totalAmount: 'Rp212.181.234'
+      }
+    ]
+  },
+  {
+    year: 2021,
+    data: [
+      {
+        month: 'January',
+        accounts: [
+          {
+            id: 'jenius',
+            name: 'Jenius',
+            amount: 'Rp10.000.000'
+          },
+          {
+            id: 'bca',
+            name: 'BCA',
+            amount: 'Rp10.200.140.234'
+          },
+          {
+            id: 'ovo',
+            name: 'OVO',
+            amount: 'Rp2.040.000'
+          },
+          {
+            id: 'gopay',
+            name: 'Gopay',
+            amount: 'Rp1.000'
+          }
+        ],
+        totalAmount: 'Rp212.181.234'
+      },
+      {
+        month: 'February',
+        accounts: [
+          {
+            id: 'jenius',
+            name: 'Jenius',
+            amount: 'Rp10.000.000'
+          },
+          {
+            id: 'bca',
+            name: 'BCA',
+            amount: 'Rp10.200.140.234'
+          },
+          {
+            id: 'ovo',
+            name: 'OVO',
+            amount: 'Rp2.040.000'
+          },
+          {
+            id: 'gopay',
+            name: 'Gopay',
+            amount: 'Rp1.000'
+          }
+        ],
+        totalAmount: 'Rp212.181.234'
+      }
+    ]
+  }
+];
+
+const _renderHistoryTabDetailContent = items =>
+  items.map((item, i) => {
+    return (
+      <View>
+        <HistoryTabDetailContent key={i} item={item} />
+      </View>
+    );
+  });
+
 export const HistoryTabDetail = props => {
   return (
     <Swiper loop={false} showsPagination={false}>
-      <View>
-        <HistoryTabDetailContent year={2019} />
-      </View>
-      <View>
-        <HistoryTabDetailContent year={2020} />
-      </View>
-      <View>
-        <HistoryTabDetailContent year={2021} />
-      </View>
+      {_renderHistoryTabDetailContent(data)}
     </Swiper>
   );
 };
