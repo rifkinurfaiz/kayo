ffb2561ef48dc999ca85a40ced48bf1a13c10593
Rifki.Nurfaiz <rifki.nurfaiz@btpn.com>
2021-08-11 21:12:52 +0700

    Remove warning

--- src/Components/BottomSheet/BottomSheet.component.js
+++ src/Components/BottomSheet/BottomSheet.component.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Fragment } from 'react';
 import { View, TouchableWithoutFeedback, Dimensions, ScrollView, Text } from 'react-native';
 import RBSheet from 'react-native-raw-bottom-sheet';
 
@@ -14,14 +15,16 @@ const Screen = {
 const _renderIcon = id => <View style={styles.icon(config.color[id])} />;
 
 const _renderAssetList = (item, i) => (
-  <View style={rbStyles.assetListContainer}>
-    <View style={rbStyles.assetList} key={i}>
-      {_renderIcon(item.id)}
-      <Text style={rbStyles.assetName} numberOfLines={2}>{item.name}</Text>
-      <Text style={rbStyles.amount}>{item.amount}</Text>
+  <Fragment key={i}>
+    <View style={rbStyles.assetListContainer}>
+      <View style={rbStyles.assetList} key={i}>
+        {_renderIcon(item.id)}
+        <Text style={rbStyles.assetName} numberOfLines={2}>{item.name}</Text>
+        <Text style={rbStyles.amount}>{item.amount}</Text>
+      </View>
+      <Text style={rbStyles.type}>{item.type}</Text>
     </View>
-    <Text style={rbStyles.type}>{item.type}</Text>
-  </View>
+  </Fragment>
 
 );
 
